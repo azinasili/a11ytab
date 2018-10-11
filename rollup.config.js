bfdc46ae7fd7e9e7ecc95590f333734bbbb6d32e
Azin Asili <azinasili@gmail.com>
2018-10-10 21:23:44 -0400

    cleaned up styling

--- rollup.config.js
+++ rollup.config.js
@@ -3,9 +3,7 @@ import babel from 'rollup-plugin-babel';
 import uglify from 'rollup-plugin-uglify';
 import pkg from './package.json';
 
-const capitalizeFirstLetter = (string) => {
-  return string.charAt(0).toUpperCase() + string.slice(1);
-}
+const capitalizeFirstLetter = string => string.charAt(0).toUpperCase() + string.slice(1);
 
 const banner = `/*!
  * ${capitalizeFirstLetter(pkg.name)} v${pkg.version}
@@ -17,8 +15,17 @@ const banner = `/*!
 export default {
   input: `src/${pkg.name}.js`,
   output: [
-    { banner, file: pkg.main, format: 'umd', name: `${capitalizeFirstLetter(pkg.name)}` },
-    { banner, file: pkg.module, format: 'es' },
+    {
+      banner,
+      file: pkg.main,
+      format: 'umd',
+      name: `${capitalizeFirstLetter(pkg.name)}`,
+    },
+    {
+      banner,
+      file: pkg.module,
+      format: 'es',
+    },
   ],
   plugins: [
     eslint({
